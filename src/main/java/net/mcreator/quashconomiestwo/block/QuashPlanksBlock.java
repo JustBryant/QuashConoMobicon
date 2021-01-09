bf2f45ee2c3d30b9226daf767b6766a8d3c910f2
JakeBat-H <unconfigured@null.spigotmc.org>
2021-01-09 12:46:38 -0400

    logs n planks

---
+++ src/main/java/net/mcreator/quashconomiestwo/block/QuashPlanksBlock.java
@@ -0,0 +1,57 @@
+
+package net.mcreator.quashconomiestwo.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.quashconomiestwo.itemgroup.QuashconomiesItemGroup;
+import net.mcreator.quashconomiestwo.QuashconomiestwoModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@QuashconomiestwoModElements.ModElement.Tag
+public class QuashPlanksBlock extends QuashconomiestwoModElements.ModElement {
+	@ObjectHolder("quashconomiestwo:quash_planks")
+	public static final Block block = null;
+	public QuashPlanksBlock(QuashconomiestwoModElements instance) {
+		super(instance, 77);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(QuashconomiesItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.AXE));
+			setRegistryName("quash_planks");
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.GRASS;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
