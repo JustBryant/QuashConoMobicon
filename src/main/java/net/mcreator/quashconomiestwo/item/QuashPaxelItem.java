24b4aabdee484f8f9a583b4520729ede92d4cee5
JakeBat-H <unconfigured@null.spigotmc.org>
2021-01-15 13:34:20 -0400

    paxel

---
+++ src/main/java/net/mcreator/quashconomiestwo/item/QuashPaxelItem.java
@@ -0,0 +1,89 @@
+
+package net.mcreator.quashconomiestwo.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.quashconomiestwo.itemgroup.QuashconomiesItemGroup;
+import net.mcreator.quashconomiestwo.QuashconomiestwoModElements;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
+
+@QuashconomiestwoModElements.ModElement.Tag
+public class QuashPaxelItem extends QuashconomiestwoModElements.ModElement {
+	@ObjectHolder("quashconomiestwo:quash_paxel")
+	public static final Item block = null;
+	public QuashPaxelItem(QuashconomiestwoModElements instance) {
+		super(instance, 118);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemToolCustom() {
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("quash_paxel"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			super(new Item.Properties().group(QuashconomiesItemGroup.tab).maxDamage(2900));
+		}
+
+		@Override
+		public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+				builder.putAll(super.getAttributeModifiers(equipmentSlot));
+				builder.put(Attributes.ATTACK_DAMAGE,
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 6f, AttributeModifier.Operation.ADDITION));
+				builder.put(Attributes.ATTACK_SPEED,
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
+				return builder.build();
+			}
+			return super.getAttributeModifiers(equipmentSlot);
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return 4 >= state.getHarvestLevel();
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+			return 10f;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+			stack.damageItem(1, attacker, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+			stack.damageItem(1, entityLiving, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 26;
+		}
+	}
+}
